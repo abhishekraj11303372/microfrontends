353bde09e29dd7ab4df5787fd34c6f1339204b30
Abhishek Raj <abhishekraj1130@gmail.com>
2022-05-18 00:50:57 +0530

    prod container setup

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,7 +1,6 @@
 const {merge} = require('webpack-merge');
 const packageJson = require('../package.json');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common');
 
@@ -22,9 +21,6 @@ const devConfig = {
             shared: packageJson.dependenciesZ,  
             // shared: ['react','react-dom'],  
         }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
     ]
 };
 
