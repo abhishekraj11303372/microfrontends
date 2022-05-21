4d70df8d77d66bb18c325e406209f9631768bd9f
Abhishek Raj <abhishekraj1130@gmail.com>
2022-05-22 01:38:28 +0530

    history navigaton

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,19 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { createMemoryHistory } from 'history'
 
-const mount = (el) => {
+const mount = (el, {onNavigate}) => {
+    const history = createMemoryHistory(); 
+    
+    if(onNavigate){
+        history.listen(onNavigate);
+    }
+
   ReactDOM.render(
-      <App />,
+      <App history={history} />,
       el
   );
+
+  return{
+    onParentNavigate({pathname: nextPathname}){
+        const {pathname} = history.location;
+
+        if(pathname != nextPathname){
+            history.push(nextPathname);
+        }
+    }
+  };
 };
 
 if(process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('._marketing-dev-root');
 
     if(devRoot) {
-        mount(devRoot);
+        mount(devRoot, {});
     }
 }
 
