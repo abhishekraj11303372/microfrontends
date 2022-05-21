87665e3ac0192ecc719f79c97a52989a73ccb3a6
Abhishek Raj <abhishekraj1130@gmail.com>
2022-05-21 18:29:33 +0530

    css randomize class names for collison

--- container/src/App.js
+++ container/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
 import { BrowserRouter } from 'react-router-dom'
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
+
 function App() {
   return (
     <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
       <div>
         <Header />
         <MarketingApp />
       </div>
+      </StylesProvider>
     </BrowserRouter>
   )
 }
