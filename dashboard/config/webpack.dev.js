77c49a01960f58c116abc04a459f085b2cb82d1a
Abhishek Raj <abhishekraj1130@gmail.com>
2022-05-22 23:42:15 +0530

    dashboard init

---
+++ dashboard/config/webpack.dev.js
@@ -0,0 +1,37 @@
+const {merge} = require('webpack-merge');
+const packageJson = require('../package.json');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const commonConfig = require('./webpack.common');
+
+const devConfig = {
+    mode: 'development',
+    output: {
+        publicPath: 'http://localhost:8085/'
+    },
+    devServer: {
+        port: 8085,
+        historyApiFallback: {
+            index: 'index.html'
+        },
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        }
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'dashboard',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './DashboardApp': './src/bootstrap',
+            },
+            shared: packageJson.dependencies,  
+            // shared: ['react','react-dom'],  
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+    ],
+};
+
+module.exports = merge(commonConfig,devConfig);
