86c1cf25e90354b21bd73e3c9a0f2c2bbd05745a
Abhishek Raj <abhishekraj1130@gmail.com>
2022-05-23 00:00:05 +0530

    no options history needed for vue

--- dashboard/src/bootstrap.js
+++ dashboard/src/bootstrap.js
@@ -10,7 +10,7 @@ if(process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('.dashboard-dev-root');
 
     if(devRoot) {
-        mount(devRoot, {defaultHistory: createBrowserHistory});
+        mount(devRoot);
     }
 }
 
